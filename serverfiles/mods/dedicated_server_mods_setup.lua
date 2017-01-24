c03d40ec0a444b76ff008bdd7369998b266726d4
6yp4z5 <bypazs.fizs@gmail.com>
2017-01-24 13:50:51 +0700

    delete
    dit work!!

--- serverfiles/mods/dedicated_server_mods_setup.lua
+++ serverfiles/mods/dedicated_server_mods_setup.lua
@@ -9,19 +9,22 @@
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
 	--ServerModCollectionSetup("379114180")
+	
+--utility	
 ServerModSetup("356930882") --Infinite Tent Uses
 ServerModSetup("380423963") --Mineable Gems
 ServerModSetup("462434129") --Restart
 ServerModSetup("661253977") --Don't Starve Together
 ServerModSetup("700236083") --Quick Actions
 ServerModSetup("597417408") --Less lags
-ServerModSetup("361994110") --Bee Nice
 ServerModSetup("378160973") --Global Positions
 ServerModSetup("375859599") --Health Info
 ServerModSetup("347079953") --Display Food Values
 ServerModSetup("791937426") --Faster Farm
 ServerModSetup("722788575") --Your Rules!
+ServerModSetup("679636739") --Trap Reset
 
+--character
 ServerModSetup("379628839") --The Medic
 ServerModSetup("498729626") --Annie Silhcade Edition
 ServerModSetup("461627386") --Madoka Kaname
@@ -30,8 +33,7 @@ ServerModSetup("791838548") --The Engineer
 ServerModSetup("781708540") --Caopr
 ServerModSetup("828661813") --Ninja Brian
 ServerModSetup("839280973") --Erik The Tennis Player
-
 ServerModSetup("591258809") --Elphelt Valentine
 ServerModSetup("847991972") --Kiana Kaslana DST
-ServerModSetup("353697884") --Craftable Gears For DST
-ServerModSetup("679636739") --Trap Reset
+
+
