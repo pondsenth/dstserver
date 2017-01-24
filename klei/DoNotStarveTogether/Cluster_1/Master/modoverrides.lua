b7e4496d595a247385b7f109af3a8477ec46d48c
6yp4z5 <bypazs.fizs@gmail.com>
2017-01-24 11:48:57 +0700

    edit position code
    edit and check

--- klei/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
+++ klei/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -1,14 +1,14 @@
 return{ -- { return
-["workshop-462434129"] = { --Restart
-	enabled = true,
-	configuration_options =
-	{
-	["MOD_RESTART_CD_RESTART"] = 0,
-	["MOD_RESTART_CD_RESURRECT"] = 0,
-	["MOD_RESTART_CD_KILL"] = 0,
-	["MOD_RESTART_CD_RESURRECT_HEALTH"] = 2,
-	}
+	["workshop-462434129"] = { --Restart
+		enabled = true,
+		configuration_options =
+		{
+			["MOD_RESTART_CD_RESTART"] = 0, --unlimitted
+			["MOD_RESTART_CD_RESURRECT"] = 0, --unlimitted
+			["MOD_RESTART_CD_KILL"] = 0, --unlimitted
+			["MOD_RESTART_CD_RESURRECT_HEALTH"] = 2, --random health
+		}
 
-} Restart
+	} --Restart
 
-} Restart
+} --return
